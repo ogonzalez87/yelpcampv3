b49293bdee4db44b705fef5ce6da02c79ee2b7e9
Oscar Gonzalez <ofgonzalez87@gmail.com>
2017-10-21 22:23:20 +0000

    v2

--- app.js
+++ app.js
@@ -1,49 +1,102 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+//mongoose.Promise = global.Promise;    
+mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});      
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-];
-    
+
+// Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground",campgroundSchema);
+
+// Campground.create(
+//     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//         description: "This is a huge granite hill, no bathrooms. No water. Beautifull granite."
+//     }
+//     ,function(err,campground){
+//         if(err){
+//             console.log(err);
+//         }else{
+//             console.log("New created campground");
+//             console.log(campground);
+//         }
+//     });
+
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
+//IDNEX - Show all campgrounds
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+  //Get campgrounds from db
+  Campground.find({},function(err,allCampgrounds){
+      if(err){
+          console.log(err);
+      }else{
+        res.render("index",{campgrounds:allCampgrounds});
+          
+      }
+  });
 });
 
+//CREATE - Add new campbrounds to DB
 app.post("/campgrounds", function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description:desc}
+    //Create a new campground and save to db
+    Campground.create(newCampground,function(err,newlyCreate){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
+//NEW - Show form to create campgrounds
 app.get("/campgrounds/new", function(req, res){
    res.render("new.ejs"); 
 });
 
+//SHOWS - Shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with the provided id
+    
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if (err){
+            console.log(err);
+        } else{
+            //render show template with that campground
+            res.render("show",{campground: foundCampground});
+        }
+    });
+
+    
+});
+
+
 //For C9 Use This!!!!
-// app.listen(process.env.PORT, process.env.IP, function(){
+ app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp Server Has Started!");
+ });
+
+//app.listen(3000 , function(){
 //    console.log("The YelpCamp Server Has Started!");
 // });
 
-app.listen(3000 , function(){
-    console.log("The YelpCamp Server Has Started!");
- });
+
+//If mongo stops working:
+//killall mongod ; cd ; ./mongod --repair ; cd data ; rm -rf mongod.lock ; cd ; ./mongod
